0ecd6dab15496199b149d4d51505e339eac8dde1
alejandro gg <gaojizhong@gmail.com>
2017-02-21 20:08:03 -0600

    learning practicing commiting what's inside unstage... second time commiting here hehe....

---
+++ mi_forms.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+print('didnt crash... good')
+
+from flask import Flask, request, render_template, jsonify
+import requests
+import logging
+app = Flask(__name__)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    app.run(debug = True)
